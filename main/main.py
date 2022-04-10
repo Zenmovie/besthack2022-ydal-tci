c8bf0aa3a3c1f81447904e98c61aae6b0ced442c
m.saidov <m.saidov@rt-solar.ru>
2022-04-10 14:01:36 +0300

    #version 0.11 fixed some bugs

--- main/main.py
+++ main/main.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/login')
 def main_page():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def auth_page():
     return render_template('auth.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def auth_page():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
