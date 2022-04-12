a02d5495eead7b0338cd198f028b024e90e3f5df
kimdaniil <kimdaniil2003r@gmail.com>
2022-04-12 18:51:40 +0300

    changed API

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -2,36 +2,57 @@ import time
 from alpha_vantage.timeseries import TimeSeries
 from datetime import datetime
 import pandas as pd
+import yfinance as yf
+import numpy as np
 import plotly.graph_objects as go
 
 start = datetime.now()
 
-key = 'TVXR0BDVKJAVY9ZZ'
-base_url = 'https://www.alphavantage.co/query?'
-ts = TimeSeries(key=key, output_format='pandas')
-
-
 def make_candlestick(symbol: str):
-    data, meta_data = ts.get_intraday(symbol=symbol, interval="5min", outputsize='full')
-    data.to_excel('stock_data.xlsx')
-    df = pd.read_excel('stock_data.xlsx')
-    fig = go.Figure(data=[go.Candlestick(x=df['date'],
-                    open=df['1. open'],
-                    high=df['2. high'],
-                    low=df['3. low'],
-                    close=df['4. close'])])
+    data = yf.download(tickers=symbol, period='1d', interval='5m')
+    fig = go.Figure(data=[go.Candlestick(x=data.index,
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+                    close=data['Close'])])
     fig.update_layout(title=symbol)
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=5, label="5m", step="minute", stepmode="backward"),
+                dict(count=15, label="15m", step="minute", stepmode="backward"),
+                dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    fig.show()
+
+
+def make_linegraph(symbol: str):
+    data = yf.download(tickers=symbol, period='1d', interval='5m')
+    fig = go.Figure([go.Scatter(x=data.index, y=data['Close'])])
+    fig.update_layout(title=symbol)
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=5, label="5m", step="minute", stepmode="backward"),
+                dict(count=15, label="15m", step="minute", stepmode="backward"),
+                dict(count=1, label="HTD", step="hour", stepmode="todate"),
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
     fig.show()
 
 
 company_li = ['IBM', 'MSFT', 'AAPL', 'GOOG', 'AMZN', 'JPM', 'TSLA', 'TSM', 'NVDA', 'BABA']
 
-counter = 0
-for i in range(len(company_li)):
-    if counter == 5:
-        time.sleep(60)
-    make_candlestick(company_li[i])
-    counter += 1
+make_candlestick('MSFT')
 
 print(datetime.now() - start)
 
